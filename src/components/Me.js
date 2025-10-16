39ed7d864782a943e4bd9d9b90dabf6f58ac4505
Robbie Bardijn <robbie.bardijn@gmail.com>
2025-10-16 21:09:27 +0200

    position picture on mobile

--- src/components/Me.js
+++ src/components/Me.js
@@ -135,9 +135,14 @@ const MeStyled = styled.div`
     z-index: 1;
     outline: none;
     background-color: white;
-    width: 150px;
-    height: 150px;
-
+    width: 300px;
+    height: 300px;
+    
+    @media screen and (min-width: 640px) {
+      width: 150px;
+      height: 150px;
+    }
+    
     img {
       width: 100%;
       height: auto;
